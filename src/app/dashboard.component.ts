ef76e9e687f06d27a173e132a5b61ab45e5ae164
xortiz <xavier.ortiz.ch@gmail.com>
2017-04-29 16:33:47 -0400

    don't know why, missed some commas. added

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -5,7 +5,7 @@ import { HeroService } from './hero.service'
 
 @Component({
 	selector: 'my-dashboard',
-	templateUrl: './dashboard.component.html'
+	templateUrl: './dashboard.component.html',
 	styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent { 
